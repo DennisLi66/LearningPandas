e2e84845f419c7cb2161742cfd4046d4d73e4159
Dennis Li <li.dennis.beta@gmail.com>
2020-09-30 18:37:44 -0700

    class conversion

--- RevisedBookHandler/main.py
+++ RevisedBookHandler/main.py
@@ -1,23 +1,39 @@
 import pandas as pd;
 import im_exportData as imex;
 
-def obtainInfo():
-    info = {};
-    #general-info 1
-    gI = imex.createDictionaryFromFile('generalInfo.txt')
-    gI1 = (gI[0],pd.DataFrame(gI[1]));
-    info['generalInfo'] = gI1;
-    #contributor
-    gC = imex.createDictionaryFromFile('contributors.txt')
-    gC1 = (gC[0],pd.DataFrame(gC[1]));
-    info['contributors'] = gC1;
-    return info;
+def retrieveDBfromTitle(title):
+    db = 0;
+    #search generalInfo database for db
+    #using dictionary info
+    toPull = "";
+    return db;
+
+
+class dbHandler:
+    def __init__(self):
+        self.info = {};
+        #general-info 1
+        gI = imex.createDictionaryFromFile('generalInfo.txt')
+        gI1 = {gI[0]:pd.DataFrame(gI[1])};
+        self.info['generalInfo'] = gI1;
+        #contributor
+        gC = imex.createDictionaryFromFile('contributors.txt')
+        gC1 = {gC[0]:pd.DataFrame(gC[1])};
+        self.info['contributors'] = gC1;
+    def getSpecificDataFrame(self,name):
+        return self.info[name][name];
+    def getDBfromTitle(self,title):
+        db = 0;
+        frame = self.getSpecificDataFrame('generalInfo');
+        print(frame);
+        return db;
 
 def run():
     print("Loading...")
-    dbs = obtainInfo();
+    db = dbHandler();
     print("Welcome to the Book Handler Interface.")
-    print (dbs)
+    # print (db.getSpecificDataFrame('generalInfo'))
+    print(db.getDBfromTitle('Other Gibberish'))
 
 
 
